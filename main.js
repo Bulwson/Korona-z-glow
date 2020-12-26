966794f539890f2949c39b42249b13d33e428d2f
Bulwson <miki2000.63@gmail.com>
2020-12-26 18:05:21 +0100

    ja pierdole co sie dzieje

--- main.js
+++ main.js
@@ -1,43 +1,63 @@
-let xd
-let tab = [1, 2, 3]
+document.addEventListener('DOMContentLoaded', getData(), false);
+
+function drawChartConfirmed(data) {
+
+    let howmuchdays = 10
+    let tempconfirmed = 0
+    let datatodisplayconfirmed = []
+    let tempdeaths = 0
+    let datatodisplaydeaths = []
+    let fetcheddata = data.map(info => info.Confirmed)
+    data.forEach(element => {
+        element.Confirmed = element.Confirmed - tempconfirmed
+        tempconfirmed += element.Confirmed
+        datatodisplayconfirmed.push(element.Confirmed)
+        element.Deaths = element.Deaths - tempdeaths
+        tempdeaths += element.Deaths
+        datatodisplaydeaths.push(element.Deaths)
+    })
+    datatodisplayconfirmed = datatodisplayconfirmed.slice(-howmuchdays)
+    datatodisplaydeaths = datatodisplaydeaths.slice(-howmuchdays)
+    data = data.slice(-howmuchdays)
+    new Chart(document.getElementById("chart"), {
+        type: "line",
+        data: {
+            labels: data.map(info => info.Date.slice(0, 10)),
+            datasets: [
+                {
+                    label: "ile zachorowało",
+                    backgroundColor: "#FF0000",
+                    data: datatodisplayconfirmed,
+                    fill: false,
+                    pointRadius: 0.1,
+                    borderColor: "#FF0000",
+                    pointHitRadius: 5
+                },
+                {
+                    label: "ile zmarlo",
+                    backgroundColor: "#000000",
+                    data: datatodisplaydeaths,
+                    fill: false,
+                    pointRadius: 1,
+                    borderColor: "#000000",
+                    pointHitRadius: 5
+                }
+
+            ]
+        },
+        options: {
+            legend: { display: false }
+        }
+    })
+}
+
 function getData() {
     fetch('https://api.covid19api.com/total/country/poland').then(
         response => response.json().then(
             data => {
-                xd = data
-                document.getElementById('zachorowania').innerHTML = `W poprzednim dniu zachorowało: ${data[data.length - 1].Confirmed - data[data.length - 2].Confirmed}`
-                document.getElementById('smierci').innerHTML = `W poprzednim dniu zmarło: ${data[data.length - 1].Deaths - data[data.length - 2].Deaths}`
-                const ctx = document.getElementById('chart').getContext('2d')
-                let abc = xd.map(info => info.Confirmed)
-                let cbd = []
-                let temp = 0
-                abc.forEach(element => {
-                    element = element - temp
-                    temp += element
-                    cbd.push(element)
-                });
-                console.log(abc)
-                new Chart(document.getElementById("chart"), {
-                    type: 'line',
-                    data: {
-                        labels: xd.map(info => info.Date.slice(0, 10)),
-                        datasets: [
-                            {
-                                label: "Ile zmarło",
-                                backgroundColor: "#FF0000",
-                                data: cbd,
-                                fill: false
-                            }
-                        ]
-                    },
-                    options: {
-                        legend: { display: false },
-                        title: {
-                            display: true,
-                            text: 'wykres kornaswirusa'
-                        }
-                    }
-                });
+                document.getElementById('zachorowania').innerHTML = data[data.length - 1].Confirmed - data[data.length - 2].Confirmed
+                document.getElementById('smierci').innerHTML = data[data.length - 1].Deaths - data[data.length - 2].Deaths
+                drawChartConfirmed(data)
             }
         )
     )
